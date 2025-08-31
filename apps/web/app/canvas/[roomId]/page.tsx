65c687df5c7e75f9d1505fb34fe42f0437bd7e4e
Jyotishman Das <jyotishmand78@gmail.com>
2025-08-31 18:22:09 +0530

    refactor canvas component: enhance drawing functionality with mouse event handling and adjust canvas dimensions

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -3,32 +3,43 @@
 import { useEffect, useRef } from "react"
 
 export default function Canvas() {
-
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
     useEffect(() => {
-
         if (canvasRef.current) {
             const canvass = canvasRef.current;
             const ctx = canvass.getContext("2d");
 
             if (!ctx) return;
 
-            ctx.strokeRect(25, 0, 100, 100);
-            // ctx.fillStyle = 
-            ctx.strokeStyle = "rgba(255, 255, 255)"
+            let clicked = false;
+            let startX = 0;
+            let startY = 0;
 
-            // canvass.addEventListener("mouseup", (e) => {
-            //     console.log(e.clientX);
-            //     console.log(e.clientY);
-            // })
+            canvass.addEventListener("mouseup", (e) => {
+                clicked = false;
+                startX = e.clientX;
+                startY = e.clientY;
+            })
+            canvass.addEventListener("mousedown", (e) => {
+                clicked = true;
+                startX = e.clientX;
+                startY = e.clientY;
+            })
+            canvass.addEventListener("mousemove", (e) => {
+                if (clicked) {
+                    const width = e.clientX - startX;
+                    const height = e.clientY - startY;
+                    ctx.clearRect(0, 0, canvass.width, canvass.height);
+                    ctx.strokeRect(startX, startY, width, height);
+                }
+            })
         }
-
     }, [canvasRef])
 
     return (
         <div>
-            <canvas className="border border-black" ref={canvasRef}></canvas>
+            <canvas width={1024} height={1024} className="border border-black" ref={canvasRef}></canvas>
         </div>
     )
 }
