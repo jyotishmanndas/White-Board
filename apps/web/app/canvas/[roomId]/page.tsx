1b6f7d678a713c98040b5fc4bd4efe25f9eb0317
Jyotishman Das <jyotishmand78@gmail.com>
2025-09-01 22:40:18 +0530

    update canvas dimensions dynamically and integrate rough.js for shape rendering

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import { Draw } from "@/components/draw";
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Canvas() {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
 
     useEffect(() => {
+        setDimensions({ width: window.innerWidth, height: window.innerHeight })
         if (canvasRef.current) {
             Draw(canvasRef.current)
         }
     }, [canvasRef]);
 
     return (
-        <div>
-            <canvas width={1080} height={800} ref={canvasRef}></canvas>
+        <div className="flex justify-center relative">
+            <div className="absolute flex justify-center h-12 mt-5 rounded-md border w-72 border-white">
+
+            </div>
+            <canvas width={dimensions.width} height={dimensions.height} ref={canvasRef}></canvas>
         </div>
     )
 }
--- apps/web/components/draw/index.ts
+++ apps/web/components/draw/index.ts
@@ -1,6 +1,16 @@
-type Shape = { type: "Rectangle" | "Circle" | "Line" | "Triangle" | "Arrow" | "Rhombus" | "Pencil" | "Eraser" | "Text", x: number, y: number, width: number, height: number, points?: { x: number; y: number }[] }
+import rough from 'roughjs';
+import { RoughCanvas } from 'roughjs/bin/canvas';
+import { Drawable } from 'roughjs/bin/core';
+
+type ShapeType = "Rectangle" | "Circle" | "Line" | "Triangle" | "Arrow" | "Rhombus" | "Pencil" | "Eraser" | "Text";
+
+type Shape = {
+    type: ShapeType;
+    drawable: Drawable;
+};
 export function Draw(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext("2d");
+    const rc = rough.canvas(canvas)
     let existingShapes: Shape[] = [];
     if (!ctx) return;
 
@@ -10,49 +20,57 @@ export function Draw(canvas: HTMLCanvasElement) {
 
     canvas.addEventListener("mouseup", (e) => {
         clicked = false;
-        const width = e.clientX - startX;
-        const height = e.clientY - startY;
-        // const currentX = e.clientX - canvas.getBoundingClientRect().left;
-        // const currentY = e.clientY - canvas.getBoundingClientRect().top;
-        // const width = currentX - startX;
-        // const height = currentY - startY;
-        existingShapes.push({ type: "Rectangle", x: startX, y: startY, width, height });
-    })
+        // const width = e.clientX - startX;
+        // const height = e.clientY - startY;
+        const currentX = e.clientX - canvas.getBoundingClientRect().left;
+        const currentY = e.clientY - canvas.getBoundingClientRect().top;
+        const width = currentX - startX;
+        const height = currentY - startY;
+        const shape = rc.generator.rectangle(startX, startY, width, height, { stroke: "white" });
+        // existingShapes.push({ type: "Rectangle", x: startX, y: startY, width, height, shape });
+        existingShapes.push({ type: "Rectangle", drawable: shape })
+    });
     canvas.addEventListener("mousedown", (e) => {
         clicked = true;
-        startX = e.clientX;
-        startY = e.clientY;
-        // startX = e.clientX - canvas.getBoundingClientRect().left;
-        // startY = e.clientY - canvas.getBoundingClientRect().top;
+        // startX = e.clientX;
+        // startY = e.clientY;
+        startX = e.clientX - canvas.getBoundingClientRect().left;
+        startY = e.clientY - canvas.getBoundingClientRect().top;
     })
     canvas.addEventListener("mousemove", (e) => {
         if (clicked) {
-            // const currentX = e.clientX - canvas.getBoundingClientRect().left;
-            // const currentY = e.clientY - canvas.getBoundingClientRect().top;
-            const width = e.clientX - startX;
-            const height = e.clientY - startY;
+            const currentX = e.clientX - canvas.getBoundingClientRect().left;
+            const currentY = e.clientY - canvas.getBoundingClientRect().top;
+            const width = currentX - startX;
+            const height = currentY - startY;
+            // const width = e.clientX - startX;
+            // const height = e.clientY - startY;
             // ctx.clearRect(0, 0, canvas.width, canvas.height);
             // ctx.fillRect(startX, startY, width, height);
             // ctx.fillStyle = "rgba(0, 0, 0)";
-            renderShapes(existingShapes, ctx, canvas)
-            ctx.strokeStyle = "rgba(255, 255, 255)"
-            ctx.strokeRect(startX, startY, width, height);
+            renderShapes(existingShapes, ctx, canvas, rc)
+            // ctx.strokeStyle = "rgba(255, 255, 255)"
+            // ctx.strokeRect(startX, startY, width, height);
+            const preview = rc.generator.rectangle(startX, startY, width, height, { stroke: "white" });
+            rc.draw(preview);
         }
     });
 };
 
-function renderShapes(existingShapes: Shape[], ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
+function renderShapes(existingShapes: Shape[], ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement, rc: RoughCanvas) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "rgba(0, 0, 0)"
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     existingShapes.forEach(shape => {
-        switch (shape.type) {
-            case "Rectangle":
-                //    ctx.fillRect(shape.x, shape.y, shape.width, shape.height);
-                ctx.strokeStyle = "rgba(255, 255, 255)"
-                ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
-                break;
-            // Handle other shapes...
-        }
+        // switch (shape.type) {
+        //     case "Rectangle":
+        //         //    ctx.fillRect(shape.x, shape.y, shape.width, shape.height);
+        //         // ctx.strokeStyle = "rgba(255, 255, 255)"
+        //         // ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        //         rc.rectangle(shape.x, shape.y, shape.width, shape.height, { stroke: "white" })
+        //         break;
+        //     // Handle other shapes...
+        // }
+        rc.draw(shape.drawable);
     });
 }
