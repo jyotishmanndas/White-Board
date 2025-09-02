a372cab586a545576d94d9a9a018f124205bb492
Jyotishman Das <jyotishmand78@gmail.com>
2025-09-02 18:45:35 +0530

    add tooltip component and integrate with canvas for shape rendering

--- apps/web/app/canvas/[roomId]/page.tsx
+++ apps/web/app/canvas/[roomId]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Draw } from "@/components/draw";
-import { useEffect, useRef, useState } from "react"
+import { Circle, MoveRight, Pencil, Square } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@workspace/ui/components/tooltip"
 
 export default function Canvas() {
     const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -16,8 +18,39 @@ export default function Canvas() {
 
     return (
         <div className="flex justify-center relative">
-            <div className="absolute flex justify-center h-12 mt-5 rounded-md border w-72 border-white">
-
+            <div className="absolute flex justify-center items-center gap-6 h-12 mt-5 rounded-md w-72 bg-[#232329]">
+                <Tooltip delayDuration={30}>
+                    <TooltipTrigger asChild>
+                        <Square className="w-5 h-5" />
+                    </TooltipTrigger>
+                    <TooltipContent side="bottom">
+                        Square
+                    </TooltipContent>
+                </Tooltip>
+                <Tooltip delayDuration={30}>
+                    <TooltipTrigger asChild>
+                        <Circle className="w-5 h-5" />
+                    </TooltipTrigger>
+                    <TooltipContent side="bottom">
+                        Circle
+                    </TooltipContent>
+                </Tooltip>
+                <Tooltip delayDuration={30}>
+                    <TooltipTrigger asChild>
+                        <Pencil className="w-5 h-5" />
+                    </TooltipTrigger>
+                    <TooltipContent side="bottom">
+                        Pencil
+                    </TooltipContent>
+                </Tooltip>
+                <Tooltip delayDuration={30}>
+                    <TooltipTrigger asChild>
+                        <MoveRight className="w-5 h-5" />
+                    </TooltipTrigger>
+                    <TooltipContent side="bottom">
+                        arrow
+                    </TooltipContent>
+                </Tooltip>
             </div>
             <canvas width={dimensions.width} height={dimensions.height} ref={canvasRef}></canvas>
         </div>
---
+++ apps/web/components/tooltip.tsx
@@ -0,0 +1,19 @@
+// import { TooltipContent, TooltipTrigger } from "@workspace/ui/components/tooltip"
+
+// interface TooltipProps {
+//     children: React.ReactNode,
+//     label: string
+// }
+
+// export function Tooltip({ children, label }: TooltipProps) {
+//     return (
+//         <Tooltip>
+//             <TooltipTrigger asChild>
+//                 {children}
+//             </TooltipTrigger>
+//             <TooltipContent side="bottom">
+//                 {label}
+//             </TooltipContent>
+//         </Tooltip>
+//     )
+// }
